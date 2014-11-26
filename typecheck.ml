64f6fd806ff171e6e7e5b1362bc8149217c53641
arpitg1991@gmail.com <arpitg1991@gmail.com>
2014-11-26 13:44:21 -0500

    cleaning

--- typecheck.ml
+++ typecheck.ml
@@ -25,18 +25,6 @@ let get_vtype env id =
 	let t = find_variable id env in
 	if t = "" then raise (Failure ("undefined variable: " ^ id)) else t
 
-(* get the type of expression:
- *  -> string if one of the two operands having string type
- *  -> int/boolean if both of the operands having the same type *)
-let get_expr_type t1 t2 =
-	if t1 = "void" || t2 = "void" then raise (Failure ("cannot use void type inside expression")) else
-	if t1 = "string" || t2 = "string" then "string" else
-	if t1 = "int" && t2 = "int" then "int" else
-	if t1 = "bool" && t2 = "bool" then "bool" else
-	if t1 = "int" && t2 = "bool" then "int" else
-	if t1 = "bool" && t2 = "int" then "int" else
-	raise (Failure ("type error"))
-
 
 
 
@@ -47,79 +35,35 @@ let check_func_arg lst expr arg_t =
 	if (snd expr) = arg_t then (fst expr)::lst else
 	raise (Failure("function arguments do not match"))
 
-let match_oper e1 op e2 =
-	(* snd of expr is type *)
-	let expr_t = get_expr_type (snd e1) (snd e2) in
-	(match op with
-	   Ast.Add -> if expr_t = "int" then (Sast.Binop(fst e1, Sast.Add, fst e2), "int") else
-		  		raise (Failure ("type error"))
-	 | Ast.Sub -> if expr_t = "int" then (Sast.Binop(fst e1, Sast.Sub, fst e2), "int") else
-		  raise (Failure ("type error"))
-	 | Ast.Mult -> if expr_t = "int" then (Sast.Binop(fst e1, Sast.Mult, fst e2), "int") else
-	 	   raise (Failure ("type error"))
-	 | Ast.Div -> if expr_t = "int" then (Sast.Binop(fst e1, Sast.Div, fst e2), "int") else
-		  raise (Failure ("type error"))
-		  (* equal and not equal have special case for string comparison 
-		  		we may need to add SAST and Eqs and Neqs *)
-	 | Ast.Equal -> if expr_t = "int" then (Sast.Binop(fst e1, Sast.Equal, fst e2), "bool") else
-                  raise (Failure ("type error in == "))
-	 | Ast.Neq -> if expr_t = "int" then (Sast.Binop(fst e1, Sast.Neq, fst e2), "bool") else
-                  raise (Failure ("type error"))
-	 | Ast.Less ->if expr_t = "int" then (Sast.Binop(fst e1, Sast.Less, fst e2), "bool") else
-                  raise (Failure ("type error")) 
-	 | Ast.Leq ->if expr_t = "int" then (Sast.Binop(fst e1, Sast.Leq, fst e2), "bool") else
-                  raise (Failure ("type error"))
-	 | Ast.Greater ->if expr_t = "int" then (Sast.Binop(fst e1, Sast.Greater, fst e2), "bool") else
-                  raise (Failure ("type error"))
-	 | Ast.Geq ->if expr_t = "int" then (Sast.Binop(fst e1, Sast.Geq, fst e2), "bool") else
-                  raise (Failure ("type error")) 
-     | Ast.And ->if expr_t = "bool" then (Sast.Binop(fst e1, Sast.And, fst e2), "bool") else
-      			  raise (Failure ("type error in and")) 
-     | Ast.Or ->if expr_t = "bool" then (Sast.Binop(fst e1, Sast.Or, fst e2), "bool") else
-      			  raise (Failure ("type error in or")) 
-	)
 
 (* it returns the expr and its type *)
 let rec check_expr env = function
 	Ast.LitInt(i) -> 
-	(*	let _ = print_string "in int" in *)
+	
 		Sast.LitInt(i), "int"
 
 	| Ast.LitStr(s) -> 
-	(*	let _ = print_string "in string " in*)
 		Sast.LitStr(s), "string"
 		
 	| Ast.LitJson(s) -> 
-	(*	let _ = print_string "in json " in*)
 		Sast.LitJson(s), "json"
 		
 	| Ast.LitList(s) -> 
-	(*	let _ = print_string "in list " in*)
 		Sast.LitList(s), "list"
 		
 	| Ast.LitBool(s) -> 
-	(*	let _ = print_string "in bool " in*)
 		Sast.LitBool(s), "bool"
 
 	| Ast.Id(id) ->
-	(*	let _ = print_string "in iD" in*)
 		Sast.Id(id), (get_vtype env id)
 	| Ast.MainRet(i) -> Sast.MainRet(i), "return"
 	
-	| Ast.Binop(e1, op, e2) ->
-	(*	let _ = print_string "in binop" in*)
-		match_oper (check_expr env e1) op (check_expr env e2)
-
 	| Ast.Assign(id, e) ->
-	(*	let _ = print_string "in assign" in*)
 		let t = get_vtype env id in
-		(* if t = "string" then 
-		     Ast.AssignStr(id, (conv_type (check_expr env e))), "void"
-		else  *)
+		    
 		Sast.Assign(id, (get_expr_with_type env e t)), "void"
 	| Ast.Call(func, el) ->
-		(* find_function is from the symbol table *)
-		let args = find_function func env in	(* return & arguments type list from definition *)
+		let args = find_function func env in	
 		( match args with
 			[] -> raise (Failure ("undefined function " ^ func))
 			| hd::tl -> let new_list = try List.fold_left2 check_func_arg [] (List.map (check_expr env) el) tl
@@ -127,14 +71,9 @@ let rec check_expr env = function
 				    in Sast.Call(func, List.rev new_list ), hd )
 	| Ast.NoExpr -> Sast.NoExpr, "void"
 
-
-(* get expr_t(sast type) by expr(ast type) with given type
- * raise error if the expression type does match requirement, snd e has the type and fst has the expr *)
 and get_expr_with_type env expr t = 
 	let e = check_expr env expr in
-	(* added special case for the path variable *)
 	fst e
-
 
 	
 let rec check_stmt env func = function
@@ -166,7 +105,6 @@ let check_formal env formal =
 	let ret = add_local formal.vname formal.vtype env in
 	if (string_of_vtype formal.vtype) = "void" then raise (Failure("cannot use void as variable type")) else
 	if StringMap.is_empty ret then raise (Failure ("local variable " ^ formal.vname ^ " is already defined"))
-	(* update the env with locals from ret *)
 	else let env = {locals = ret; globals = env.globals; functions = env.functions } in
 	convert_to_sast_type formal env, env
 
@@ -180,7 +118,6 @@ let check_local env local =
 	let ret = add_local local.vname local.vtype env in
 	if (string_of_vtype local.vtype) = "void" then raise (Failure("cannot use void as variable type")) else
 	if StringMap.is_empty ret then raise (Failure ("local variable " ^ local.vname ^ " is already defined"))
-	(* update the env with globals from ret *)
 	else let env = {locals = ret; globals = env.globals; functions = env.functions } in
 	convert_to_sast_type local env, env
 
@@ -189,28 +126,17 @@ let rec check_locals env locals =
 	  [] -> []
 	| hd::tl -> let l, e = (check_local env hd) in (l, e)::(check_locals e tl)
 
-(* this function will return the updated formals and body as per the abstract syntax tree, the return type, name and locals *)
 let check_function env func = 
-	(* if List.length func.body = 0 then raise (Failure ("The last statement must be return statement"))
-	else if func.fname = "main" && (List.length func.formals) > 0 
-	then raise (Failure ("The main function cannot take any argument"))
-	else if  func.fname = "main" && ((func.returnType = "int") || (func.returnType = "boolean"))
-	then raise (Failure ("The main function cannot can only has type void"))
-	else  *)
+	
 	match List.hd (List.rev func.body) with
 	Return(_) ->
 	  	let env = {locals = StringMap.empty; globals = env.globals; functions = env.functions } in
-	  	(*  ret is new env *)
 		let ret = add_function func.fname func.return func.formals env in
 		if StringMap.is_empty ret then raise (Failure ("function " ^ func.fname ^ " is already defined"))
-		(* update the env with functions from ret *)
 		else let env = {locals = env.locals; globals = env.globals; functions = ret } in
-		(* check the formal arguments, returns formal list appended with their env *)
 		let f = check_formals env func.formals in
-		(* get the list of formals from f *)
 		let formals = List.map (fun formal -> fst formal) f in
 		
-		(* get the final env from the last formal *)
 		let l, env = 
 		(match f with
 			  [] -> let l = check_locals env func.fnlocals in
@@ -221,7 +147,6 @@ let check_function env func =
 		) in
 		let fnlocals = List.map (fun fnlocal -> fst fnlocal) l in
 		 (match l with
-		 	(* empty f, no fomal args *)
 	            [] -> let body = check_stmt_list env func func.body in
 	                { Sast.return = get_sast_type func.return; 
 	                  Sast.fname = func.fname; 
@@ -230,7 +155,6 @@ let check_function env func =
 	                  Sast.body = body
 	                }, env
 
-	            (* get the final env from the last formal *)
 	            | _ -> let e = snd (List.hd (List.rev l)) in
 	                   let body = check_stmt_list e func func.body in
 	                  { Sast.return = get_sast_type func.return; 
@@ -249,12 +173,9 @@ let rec check_functions env funcs =
 	  [] -> []
 	| hd::tl -> let f, e = (check_function env hd) in f::(check_functions e tl) 
 
-(* returns the global and its env *)
 let check_global env global =
- 	(*let _ = print_string "in iD" in*)
 	let ret = add_global global.vname global.vtype env in
 	if StringMap.is_empty ret then raise (Failure ("global variable " ^ global.vname ^ " is already defined"))
-	(* update the env with globals from ret *)
 	else let env = {locals = env.locals; globals = ret; functions = env.functions } in
 	convert_to_sast_type global env, env
 
@@ -264,12 +185,10 @@ let rec check_globals env globals =
 	| hd::tl -> let g, e = (check_global env hd) in (g, e)::(check_globals e tl)
 
 let check_program (globals, funcs) = 
-	(* create the default environment *)
  	let env = {	locals = StringMap.empty;
 				globals = StringMap.empty;
 				functions = StringMap.empty }
 	in
-	(* return the list of each global appended with its environments, the last global has the final env *)
 	let g = check_globals env globals in
 	(* make a list of globals *)
 	let globals = List.map (fun global -> fst global) g in
