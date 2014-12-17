9095d328d1459f81404d942c350214d90593f193
bajajabhi1 <bajajabhi1@gmail.com>
2014-12-16 22:15:59 -0500

    clean up

--- typecheck.ml
+++ typecheck.ml
@@ -313,7 +313,7 @@ let rec check_stmt env func = function
 
 	| Ast.ElemAssign(id, expr1, expr2) -> let t1 = get_vtype env id in
 											let t2 = check_expr env expr1 in
-														if not ( (t1 = "notype" && (snd t2 = "string" || snd t2 = "notype" || snd t2 = "int")) ||
+														if not ( (t1 = "notype" && (snd t2 = "string" || snd t2 = "notype" || snd t2 = "number")) ||
 														 (t1 = "json" && (snd t2 = "string" || snd t2 = "notype")) || (t1="list" && (snd t2 ="number" || snd t2 = "notype")) ) 
 															then raise (Failure("Elements of List and Json can be accessed via index and key respectively!"))
 														else
